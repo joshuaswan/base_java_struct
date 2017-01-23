3c4a1c79a81c7e8cf27ea8b12ab54ba66bd18b4b
joshuaswan <joshuaswan@sina.com>
2017-01-23 19:34:43 +0800

    [joshua]Welcome

---
+++ src/main/java/com/joshua/struct/misc/Welcome.java
@@ -0,0 +1,13 @@
+package com.joshua.struct.misc;
+
+/**
+ * Created by joshua on 1/23/17.
+ */
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Welcome to the text Data Structures");
+        System.out.println("Algorithms, and Applications in Java,");
+        System.out.println("by Sartaj Sahni");
+        System.out.println("Hope you enjoy the text.");
+    }
+}
